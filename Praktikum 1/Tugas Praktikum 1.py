b2d897d63133d827803289f4501c30937fb1380e
AlvitoYumna <91053082+AlvitoYumna@users.noreply.github.com>
2021-09-20 15:12:01 +0700

    Tugas Praktikum 1
    Tugas praktikum 1, Alvito Yumna Satrio Arianto, 065002100012
    20 09 2021

---
+++ Praktikum 1/Tugas Praktikum 1.py
@@ -0,0 +1,20 @@
+# Tugas Praktikum 1
+# Senin, 20210920
+# 065.00.21.00.012 Alvito Yumna Satrio Arianto
+"""
+Created on Tue Sep 14 11:12:12 2021
+
+065.00.21.00.012 Alvito Yumna Satrio Arianto
+
+@author: Alvito Yumna Satrio Arianto
+"""
+
+print ('Hai')
+x=input ("Masukkan nama kamu=")
+print ('Hi',x)
+
+q=input ("Kamu berasal dari mana=")
+print (q)
+
+r=input ("Kamu dari sekolah mana?= ")
+print (r)
